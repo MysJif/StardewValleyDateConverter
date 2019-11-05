11ed822ffa495e4f9dcd7bab35f4597844a4e94f
jfields <jfields@student.mchenry.edu>
2019-11-05 14:28:11 -0600

    added buttons

--- app/src/main/java/com/example/stardewvalleydateconverter/MainActivity.kt
+++ app/src/main/java/com/example/stardewvalleydateconverter/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.stardewvalleydateconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showDate = findViewById<TextView>(R.id.textShowDate)
+        val dateSpinner = findViewById<Spinner>(R.id.spinnerDate)
+        val convertBtn = findViewById<Button>(R.id.btnConvert)
+        val customBtn = findViewById<Button>(R.id.btnCustom)
+
+        convertBtn.setOnClickListener {
+            dateSpinner.
+        }
+
     }
 }
