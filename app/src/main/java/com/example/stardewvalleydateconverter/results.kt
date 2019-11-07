70591c1d4447cb2f153df43971d977f50f02a4b3
jfields <jthediamondminer@gmail.com>
2019-11-07 01:19:49 -0600

    added button code to results activity

--- app/src/main/java/com/example/stardewvalleydateconverter/results.kt
+++ app/src/main/java/com/example/stardewvalleydateconverter/results.kt
@@ -1,12 +1,30 @@
 package com.example.stardewvalleydateconverter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class results : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        val resultText = findViewById<TextView>(R.id.textResult2)
+        val backBtn = findViewById<Button>(R.id.btnBack)
+        val mainBtn = findViewById<Button>(R.id.btnMain2)
+
+        backBtn.setOnClickListener{
+            var intent = Intent(this, custom::class.java)
+            startActivity(intent)
+        }
+
+        mainBtn.setOnClickListener{
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
