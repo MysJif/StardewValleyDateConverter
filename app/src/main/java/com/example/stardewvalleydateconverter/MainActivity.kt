58acb80c4e29df644b8b655061b8bf101fab69e0
jfields <jfields@student.mchenry.edu>
2019-11-21 14:54:59 -0600

    fixed warning

--- app/src/main/java/com/example/stardewvalleydateconverter/MainActivity.kt
+++ app/src/main/java/com/example/stardewvalleydateconverter/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Spinner
 import android.widget.TextView
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.temporal.TemporalAdjusters
 
 class MainActivity : AppCompatActivity() {
@@ -25,18 +24,18 @@ class MainActivity : AppCompatActivity() {
         showDate.text = convert(LocalDate.now())
 
         convertBtn.setOnClickListener {
-            var id = dateSpinner.selectedItemPosition
+            val id = dateSpinner.selectedItemPosition
             showDate.text = convertSet(id)
         }
 
         customBtn.setOnClickListener{
-            var intent = Intent(this, custom::class.java)
+            val intent = Intent(this, custom::class.java)
             startActivity(intent)
         }
 
     }
 
-    fun convertSet(id: Int): String {
+    private fun convertSet(id: Int): String {
         when(id) {
             0->{
                 return convert(LocalDate.now())
@@ -45,13 +44,13 @@ class MainActivity : AppCompatActivity() {
                 return convert(LocalDate.of(LocalDate.now().year, 1, 1 ))
             }
             2->{
-                return convert(Easter(LocalDate.now().year))
+                return convert(easter(LocalDate.now().year))
             }
             3->{
                 return convert(LocalDate.of(LocalDate.now().year, 10, 31))
             }
             4->{
-                return convert(LocalDate.of(LocalDate.now().year, 11, Thanksgiving(LocalDate.now().year)))
+                return convert(LocalDate.of(LocalDate.now().year, 11, thanksgiving(LocalDate.now().year)))
             }
             5->{
                 return convert(LocalDate.of(LocalDate.now().year, 12, 25))
@@ -62,31 +61,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun Thanksgiving(year: Int): Int { //thanksgiving! why arent these static.
-        var first = LocalDate.of(year, 11, 1)
+    private fun thanksgiving(year: Int): Int { //thanksgiving! why arent these static.
+        val first = LocalDate.of(year, 11, 1)
+        val thanks: Int
 
         if (first.dayOfWeek.equals(DayOfWeek.THURSDAY)) {
-            return first.plusWeeks(3).dayOfMonth
+            thanks = first.plusWeeks(3).dayOfMonth
         } else {
-            return first.with(TemporalAdjusters.next(DayOfWeek.THURSDAY)).plusWeeks(3).dayOfMonth
+            thanks = first.with(TemporalAdjusters.next(DayOfWeek.THURSDAY)).plusWeeks(3).dayOfMonth
         }
+
+        return thanks
     }
 
-    fun Easter(year: Int): LocalDate { //i dont understand this and i dont want to understand this. ask gauss.
-        var a = year%19
-        var b = year/100
-        var c = year%100
-        var d = b/4
-        var e = b%4
-        var f = (b+8)/25
-        var g = (b-f+1)/3
-        var h = (19*a+b-d-g+15)%30
-        var i = c/4
-        var k = c%4
-        var m = (32+2*e+2*i-h-k)%7
-        var n = (a+11*h+22*m)/451
-        var month = (h+m-7*n+114)/31
-        var day = (((h+m-(7*n)+114)%31)+1)
+    private fun easter(year: Int): LocalDate { //i dont understand this and i dont want to understand this. ask gauss.
+        val a = year%19
+        val b = year/100
+        val c = year%100
+        val d = b/4
+        val e = b%4
+        val f = (b+8)/25
+        val g = (b-f+1)/3
+        val h = (19*a+b-d-g+15)%30
+        val i = c/4
+        val k = c%4
+        val m = (32+2*e+2*i-h-k)%7
+        val n = (a+11*h+22*m)/451
+        val month = (h+m-7*n+114)/31
+        val day = (((h+m-(7*n)+114)%31)+1)
 
         return LocalDate.of(year, month, day)
     }
